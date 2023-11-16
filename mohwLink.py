78505a148769284621f2be4f256775d463c04d30
Bright Su <gt07814@greattree.com.tw>
2023-11-16 17:34:30 +0800

    feat: add payload info

--- mohwLink.py
+++ mohwLink.py
@@ -28,6 +28,9 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 }
 requests.packages.urllib3.disable_warnings()
-baseSessionId=requests.request('GET',baseUrl,headers=headers ,verify=False)
-getSessionId=re.search(r'ASP.NET_SessionId=([a-zA-Z0-9]+);',baseSessionId.headers['Set-Cookie']).group(1)
+baseSession=requests.request('GET',baseUrl,headers=headers ,verify=False)
+getSessionId=re.search(r'ASP.NET_SessionId=([a-zA-Z0-9]+);',baseSession.headers['Set-Cookie']).group(1)
+getVIEWSTATE=re.search(r'ASP.NET_SessionId=([a-zA-Z0-9]+);',baseSession.headers['Set-Cookie']).group(1)
+getEVENTVALIDATION=re.search(r'ASP.NET_SessionId=([a-zA-Z0-9]+);',baseSession.headers['Set-Cookie']).group(1)
+getVIEWSTATEGENERATOR=re.search(r'ASP.NET_SessionId=([a-zA-Z0-9]+);',baseSession.headers['Set-Cookie']).group(1)
 print (getSessionId)
